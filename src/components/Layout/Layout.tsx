6328a9352099180522adf41d3b131497fbd3d49f
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-01 16:59:07 +0100

    Add layout component so Header and Footer persist across all pages

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import { Flex, Box } from '@chakra-ui/react';
+import { Header } from '../Header/Header';
+import { Footer } from '../Footer/Footer';
+
+type Props = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <Flex h='100vh' direction='column'>
+      <Header />
+      <Box flex='1'>{children}</Box>
+      <Footer />
+    </Flex>
+  );
+};
+export default Layout;
