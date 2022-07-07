227853707de8f1886d26134e2b19de61a7486695
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-07 10:12:31 +0100

    Update projects page with View Projects title

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from 'next';
-import { Box, Text } from '@chakra-ui/react';
 import { HeadComponent } from '../src/components/HeadComponent/HeadComponent';
+
+import { Box, Text } from '@chakra-ui/react';
+import { Project } from '../src/components/Project/Project';
 
 const Projects: NextPage = () => {
   return (
     <>
       <HeadComponent contentTitle='Usama Lodhi | Front End Web Developer' contentDescription='Portfolio Projects' />
       <Box>
-        <Text as='h1'>This is the projects page</Text>
-        <Text as='p'>This is the projects page text</Text>
+        <Text as='h1'>View Projects</Text>
       </Box>
+      <Project />
     </>
   );
 };
