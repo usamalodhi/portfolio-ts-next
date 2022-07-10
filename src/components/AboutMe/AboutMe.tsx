ce99e392d450b65e50fbb5da27f4e5d263bce5b2
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-10 08:55:10 +0100

    Update AboutMe with Highligher and about text

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,10 +1,25 @@
-import { SimpleGrid, Image, Flex, Text, Stack, StackDivider, Divider } from '@chakra-ui/react';
+import { SimpleGrid, Image, Flex, Text, Stack, Box, Divider, Button, Link } from '@chakra-ui/react';
 import test from '../../../public/assets/AboutImage.jpeg';
 
 interface FeatureProps {
-  service: string;
+  service: JSX.Element;
   text: string;
 }
+
+interface HighlighterProps {
+  backgroundColor: string;
+  children: string;
+}
+
+const HIGHLIGHT = ['#A7F3D0', '#BAE6FD', '#C7D2FE', '#FDE68A'];
+
+const Highlighter = ({ backgroundColor, children }: HighlighterProps) => {
+  return (
+    <Text as='span' fontWeight='bold' backgroundColor={backgroundColor}>
+      {children}
+    </Text>
+  );
+};
 
 const Feature = ({ service, text }: FeatureProps) => {
   return (
@@ -18,29 +33,73 @@ const Feature = ({ service, text }: FeatureProps) => {
 
 export function AboutMe() {
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing='24px' padding='16px 24px'>
+    <SimpleGrid
+      textAlign='justify'
+      wordBreak='break-word'
+      columns={{ base: 1, md: 2 }}
+      spacing='24px'
+      padding='16px 24px'
+    >
       <Stack>
-        <Text as='h1'>About Me</Text>
+        <Flex direction='column'>
+          <Box>
+            <Text as='h1'>About Me</Text>
+          </Box>
+          <Flex gap='24px'>
+            <Link textDecoration='none' href='/files/BooleanCertificate.pdf' target='_blank' rel='noopener noreferrer'>
+              <Button>View CV</Button>
+            </Link>
+            <Link href='/files/BooleanCertificate.pdf' target='_blank' rel='noopener noreferrer'>
+              <Button>View Certificate</Button>
+            </Link>
+          </Flex>
+        </Flex>
         <Text as='p'>
-          I'm a graduate of the Boolean Software Developer Academy learning the fundamentals of frontend and backend web
-          development. A strong emphasis was placed on programming in Javascript/Typescript to build full stack
-          applications using React, Node/Express, PostgreSQL and Prisma ORM.
+          I'm a graduate of the{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[0]} children='Boolean Software Developer Academy ' />
+          learning the fundamentals of frontend and backend web development. A strong emphasis was placed on programming
+          in <Highlighter backgroundColor={HIGHLIGHT[1]} children='Javascript/Typescript' /> to build full stack
+          applications using <Highlighter backgroundColor={HIGHLIGHT[2]} children='React,' />{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[3]} children='Node/Express,' />{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[0]} children='PostgresSQL' /> and{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[2]} children='Prisma ORM.' />
         </Text>
         <Text as='p'>
-          Outside of the academy I've been bolstering my skills in the latest frontend technologies including frameworks
-          such as Gatsby and Next JS, and CSS Libraries including Chakra UI, Material UI and Framer Motion. I would love
-          to work with you in an exciting Frontend role.
+          Outside of the academy I've been bolstering my skills in the latest frontend technologies utilising frameworks
+          including <Highlighter backgroundColor={HIGHLIGHT[3]} children='Gatsby' /> and{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[2]} children='Next JS,' /> CSS Libraries including{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[1]} children='Chakra UI,' />{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[3]} children='Material UI' /> and{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[0]} children='Framer Motion.' /> I would love to work with you in an
+          exciting Frontend role.
         </Text>
         <Text as='p'>
-          Prior to web development, I've worked as an analyst in financial services and a PhD researcher, where I've had
-          the opportunity to work with both small and large, specialised andcross-functional teams and developed a
-          working style that is suited for a fast-paced, self-directed environment
+          Prior to web development, I've worked as an analyst in financial services; most notably in the{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[3]} children='programming of credit strategies' /> in the specialist
+          lending market and as a PhD researcher. I've had the opportunity to work with both small and large,
+          specialised and cross-functional teams and developed a working style that is suited for a{' '}
+          <Highlighter backgroundColor={HIGHLIGHT[0]} children='fast-paced, self-directed environment' />
         </Text>
         <Text as='h2'>Services</Text>
         <Stack>
-          <Feature service={'Frontend Development'} text={'write here'} />
-          <Feature service={'Backend Development'} text={'write here'} />
-          <Feature service={'UI & UX Design'} text={'write here'} />
+          <Feature
+            service={<Highlighter backgroundColor={HIGHLIGHT[2]} children='Frontend Development' />}
+            text={
+              'Design, Build and Deployment of responsive web applications made within the React eco-system of frontend frameworks (React, Gatsby, Next JS) and production ready CSS libraries with Chakra UI, Material UI, Styled Components and Framer Motion amongst my favorite tools.'
+            }
+          />
+          <Feature
+            service={<Highlighter backgroundColor={HIGHLIGHT[3]} children='Backend Development' />}
+            text={
+              'Create RESTful server-side applications to handle HTTP requests using Node/Express with connections to relational data stores such as PostgresSQL.'
+            }
+          />
+          <Feature
+            service={<Highlighter backgroundColor={HIGHLIGHT[1]} children='UI & UX Design' />}
+            text={
+              'A keen eye for the latest trends in web development design to create engaging and user-friendly interfaces with prototyping in design tools including Figma.'
+            }
+          />
         </Stack>
       </Stack>
       <Flex>
