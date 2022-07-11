ff6196d3c89d022a343c75d67f870ffb70b17b81
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-11 20:47:17 +0100

    Update Hero with animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,27 +1,50 @@
-import { Text, Button, Stack, Flex } from '@chakra-ui/react';
+import { Text, Flex, Box } from '@chakra-ui/react';
 import { HeroProps } from './HeroInterface';
+const { motion } = require('framer-motion');
+import { VARIANTS } from '../../constants/constants';
+import Link from 'next/link';
 
 const HeroText = ({ heading, headingSpan, subHeading }: HeroProps) => {
   return (
     <>
-      <Text as='h1'>
+      <Text pb='1em' as='h1'>
         {heading} <br />
         <Text as={'span'}>{headingSpan}</Text>
       </Text>
-      <Text as='h2'>{subHeading}</Text>
+      <Text pb='1em' as='h2'>
+        {subHeading}
+      </Text>
     </>
   );
 };
 
 export function Hero() {
   return (
-    <Flex layerStyle='Hero'>
+    <Flex
+      as={motion.div}
+      variants={VARIANTS}
+      initial='hidden'
+      animate='enter'
+      exit='exit'
+      layerStyle='Hero'
+      background='linear-gradient(to right, #FFFFFF, #FFEFBA)'
+    >
       <HeroText
         heading={`Hi there, I'm Usama`}
         headingSpan={`I'm a Frontend Developer`}
         subHeading='I love building responsive web applications that are user-friendly, simple and delightful'
       />
-      <Button>View Projects</Button>
+      <Box
+        as={motion.div}
+        whileHover={{ color: '#000000', backgroundColor: '#FFFAFA' }}
+        transition='0.05s linear'
+        color='brand.secondary'
+        width='max-content'
+        textStyle='ButtonStyle'
+        layerStyle='ButtonStyle'
+      >
+        <Link href='/projects'>View Projects</Link>
+      </Box>
     </Flex>
   );
 }
